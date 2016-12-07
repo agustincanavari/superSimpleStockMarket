1c63814a547dea44353d4ae80734d8c801aa4cc4
agustincanavari <agustincanavari@gmail.com>
2016-12-07 11:25:57 -0300

    Added validations for user inputs and calculations

--- SuperSimpleStockMarket.py
+++ SuperSimpleStockMarket.py
@@ -36,13 +36,16 @@ def calculateDividendYield(stockElement, price):
   if stockElement.properties_['Type'] == "Common":
       divYield = float(stockElement.properties_['Last Dividend'])/price
   else:
-    divYield = float(stockElement.properties_['Fixed Dividend'])*float(stockElement.properties_['Par Value'])/price
+    divYield = float(stockElement.properties_['Fixed Dividend'].strip("%"))*float(stockElement.properties_['Par Value'])/(100*price)
   return divYield
 
 def calculatePERatio(stockElement, price):
   #Receives a StockElemente object and a price
-  #Returns the value of P/E Ratio     
-  return price/float(stockElement.properties_['Last Dividend'])
+  #Returns the value of P/E Ratio 
+  if float(stockElement.properties_['Last Dividend']) == 0:
+    return ("Last Dividend is 0, P/E Ratio can not be computed\n")
+  else:    
+    return price/float(stockElement.properties_['Last Dividend'])
   
 def calculateVWSP(tradesArray):
   #Receives an array with Trade objects
@@ -101,10 +104,12 @@ def extractTradesTime(tradesArray, minutes):
   return tradesAux    
    
 def showTradeArray(array):
+  #Prints values of a trade array  
   for record in array:
     showTrade(record)    
   
 def showTrade(record):
+  #Prints values of a trade 
   print(record.symbol_)
   print(record.bs_)
   print(record.quantity_)
@@ -112,15 +117,18 @@ def showTrade(record):
   print(record.timestamp_)   
 
 def showStockArray(stockArrayAux):
+  #prints values of a stockElement array 
   for stockElement in stockArrayAux:
     showStock(stockElement)    
   
 def showStock(stockElement):
+  #Prints values of a stockElement  
   print (stockElement.symbol_)
   print (stockElement.properties_)    
    
 if __name__ == '__main__':
     
+    #Data arrays
     stockArray = []
     symbolArray = []
     tradeRecordArray = []    
@@ -133,7 +141,8 @@ if __name__ == '__main__':
     while True: 
       options=list(mainMenu.keys())
       options.sort()
-      for entry in options: 
+      for entry in options:
+        print("\n")  
         print (entry, mainMenu[entry])
       selection=input("Please Select an option from the menu: ") 
       if selection =='1':
@@ -141,14 +150,13 @@ if __name__ == '__main__':
         if len(stockArray) == 0:  
           stockArray=loadStocks()  
         else:
-          while True:  
-            answer=input("Loading a new file will erase previous information, do you want to continue? (y/n): ") 
-            if answer=='y':
-              stockArray=loadStocks()
-            elif answer=='n':
-              break
-            else:
-              print("Unkwon option selected!")    
+          answer=input("Loading a new file will erase previous information, do you want to continue? (y/n): ")
+          while answer not in ['y','n']:
+            answer = input("Please enter only 'y' or 'n' for yess or no: ")
+          if answer=='y':
+            stockArray=loadStocks()
+          elif answer=='n':
+            break
       elif selection == '2':  
         #Select a stock to operate with  
         if len(stockArray) == 0:
@@ -165,7 +173,8 @@ if __name__ == '__main__':
             while True: 
               options=list(operationsMenu.keys())
               options.sort()
-              for entry in options: 
+              for entry in options:
+                print("\n")  
                 print (entry, operationsMenu[entry])
               selection=input("Please Select an operation from the menu: ")
               if selection == '1':
@@ -174,7 +183,18 @@ if __name__ == '__main__':
                   if selectedStock == stockArray[i].symbol_:
                     stockElementAux = stockArray[i]
                     break
-                price = input("Enter the price for the dividend yield calculation: ")
+                while True:
+                  try:
+                    price = float(input("Enter price for the dividend yield calculation: "))    
+                  except ValueError:
+                    print("Please check the quantity you are entering is a float ")
+                    continue
+                  else:
+                    if price <= 0:
+                      print("Please enter a positive price")  
+                      continue
+                    else:
+                      break    
                 print("Result is: ",calculateDividendYield(stockElementAux, float(price)))
               elif selection == '2':
                 #Calculate P/E Ratio
@@ -182,19 +202,48 @@ if __name__ == '__main__':
                   if selectedStock == stockArray[i].symbol_:
                     stockElementAux = stockArray[i]
                     break
-                price = input("Enter the price for the dividend yield calculation: ")
+                while True:
+                  try:
+                    price = float(input("Enter price for the P/E Ratio calculation: "))    
+                  except ValueError:
+                    print("Please check the quantity you are entering is a float ")
+                    continue
+                  else:
+                    if price <= 0:
+                      print("Please enter a positive price")  
+                      continue
+                    else:
+                      break    
                 print("Result is: ",calculatePERatio(stockElementAux, float(price)))
               elif selection == '3':
                 #Record a trade                
                 bs = input("Enter 'b' for buy or 's' for sell: ")
-                while bs != 'b' and bs != 's':
+                while bs not in ['b','s']:
                   bs = input("Please enter only 'b' or 's' for buy or sell: ")  
-                quantity = int(input("Enter the quantity to buy or sell: "))
-                while (not isinstance(quantity,int)) or quantity <= 0:
-                  quantity = input("Please enter only positive integers for this value: ")
-                price = float(input("Enter price of the operation: "))
-                while (not isinstance(price,float)) or price <= 0:
-                  price = input("Please enter only positive floats for this value: ")
+                while True:
+                  try:
+                    quantity = int(input("Enter the quantity to buy or sell: "))    
+                  except ValueError:
+                    print("Please check the quantity you are entering is an integer ")
+                    continue
+                  else:
+                    if quantity <= 0:
+                      print("Please check the quantity you are entering is positive")
+                      continue
+                    else:
+                      break
+                while True:
+                  try:
+                    price = float(input("Enter price of the operation: "))    
+                  except ValueError:
+                    print("Please check the quantity you are entering is a float ")
+                    continue
+                  else:
+                    if price <= 0:
+                      print("Please enter a positive price")  
+                      continue
+                    else:
+                      break    
                 tradeRecordArray.append(Trade(selectedStock, time.time(), quantity, bs, price))
                 tradeAux = Trade(selectedStock, time.time(), quantity, bs, price)
                 showTrade(tradeAux)
